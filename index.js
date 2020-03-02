c0f08583979b39ac12373eafbaf04c304dbd917d
Frank Schulz <Frank-Schulz@live.nmit.ac.nz>
2020-03-02 16:56:20 +1300

    Added index.js, began coding with JS

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("Hello World!");
+
+var firstName;
+firstName = "Frank";
+console.log(firstName)
+
+var lastName = "Schulz";
+console.log(lastName)
+
+console.log(firstName, lastName)
+
+//This is a single line comment
+
+/*This is a
+multi-line comment */
+
+var numberOne = 3;
+var numberTwo = 7;
+
+console.log(numberOne+numberTwo);
+
+var sum = numberOne+numberTwo;
+console.log("Sum =",sum);
+
+var sub = numberOne-numberTwo;
+console.log("Sub =",sub);
+
+var div = numberOne/numberTwo;
+console.log("Div =",div);
+
+var rem = numberOne%numberTwo;
+console.log("Rem =",rem)
