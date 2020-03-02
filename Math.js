3e70540532ed23ce62f7c9b94217342101fec2e8
Frank Schulz <Frank-Schulz@live.nmit.ac.nz>
2020-03-03 10:29:32 +1300

    Math Time!

---
+++ Math.js
@@ -0,0 +1,18 @@
+var numOne = 3
+var numTwo = 8
+var numThree = 4
+var numFour = 7
+
+var X
+
+X = numOne / numTwo + numThree / numOne - numFour % numFour
+
+console.log(X)
+
+console.log(numOne + numTwo * numThree);
+
+console.log(numOne + (numTwo * numThree));
+
+console.log((numOne + numTwo) * numThree);
+
+console.log(numOne * numThree + numTwo * numThree);
